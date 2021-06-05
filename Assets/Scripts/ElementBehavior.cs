4bcedd585f7237319048975242a43c208cafe72c
Finalcreature <Finalnadav@gmail.com>
2021-06-05 15:02:18 +0300

    Demo orgenised

---
+++ Assets/Scripts/ElementBehavior.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElementBehavior : MonoBehaviour
+{
+    bool isSpawned;
+    public MachineEngine machine;
+    float speed;
+    static int index = 0;
+    Manager manager;
+    //bool isStopped
+    // Start is called before the first frame update
+    void Start()
+    {
+        manager = FindObjectOfType<Manager>();
+        name = machine.row.ToString();
+        speed = machine.GetSpeed();
+        isSpawned = false;
+        GetComponent<SpriteRenderer>().sprite = machine.SetSprite();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y >= -4)
+        {
+            transform.Translate(Vector3.down * speed);
+            if (machine.isStopped)
+            {
+                StartCoroutine("SetSpeed");
+            }
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        if (transform.position.y < 0 && !isSpawned && !machine.isStopped)
+        {
+            machine.NextObject(transform.position.x);
+            isSpawned = true;
+        }  
+    }
+
+    IEnumerator SetSpeed()
+    {
+     
+        yield return new WaitUntil(() => transform.position.y < 0 == true);
+        speed = machine.GetSpeed();
+
+        
+        {
+            foreach (ElementBehavior element in FindObjectsOfType<ElementBehavior>())
+            {
+                if (element.name != (machine.row).ToString())
+                {
+                    Destroy(element.gameObject);
+                }
+            }
+
+            yield return new WaitForSeconds(3);
+
+            transform.position = Vector2.MoveTowards(transform.position,
+                                                     new Vector2(transform.position.x, -3), Time.deltaTime);
+            GetComponent<SpriteRenderer>().maskInteraction = SpriteMaskInteraction.None;
+
+
+            
+            yield return new WaitUntil(() => transform.position.y == -3);
+            manager.elements[index] = this;
+            index++;
+            manager.SetImages();
+            //Memory.isReadyToSet = true;
+            machine.isStopped = false;
+        }
+        
+        
+    }
+}
---
+++ Assets/Scripts/Manager.cs
@@ -0,0 +1,135 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Manager : MonoBehaviour
+{
+    public ElementBehavior[] elements = new ElementBehavior[3];
+    [SerializeField] Image[] images;
+    [SerializeField] InputField input;
+    string inventionName;
+    [SerializeField] Text example;
+    [SerializeField] GameObject panel;
+    [SerializeField] Image icon;
+    bool isShowing;
+
+    public string[] inventions = {"Solar-based Drone", "Wind Energy Technician", "Urban Agriculture Specialis",
+                            "Organizational Disrupter", "Urban Security Coordinator",
+                            "Hyper-Intelligent Transportation Engineer", "Pharmaceutical Artisan",
+                            "Extinct Species Revivalist", "Personal Education Guide", "Personal Health Lifestyle Guide" };
+    [SerializeField] Sprite[] icons;
+
+    private void Awake()
+    {
+        int gameStatusCount = FindObjectsOfType<Manager>().Length;
+        if (gameStatusCount > 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+
+    }
+
+        void Start()
+        {
+            isShowing = false;
+            Memory.isReadyToSet = false;
+            input.gameObject.SetActive(false);
+            panel.SetActive(false);
+
+        }
+
+    private void Update()
+    {
+        if (Memory.isReadyToSet)
+        {
+            Memory.isReadyToSet = false;
+        }
+        //    foreach (ElementBehavior element in elements)
+        //    {
+        //        if(element.machine.name == "Slot Machine")
+        //        {
+        //            images[0].sprite = element.GetComponent<SpriteRenderer>().sprite;
+        //        }
+        //        else if(element.machine.name == "Slot Machine (1)")
+        //        {
+        //            images[1].sprite = element.GetComponent<SpriteRenderer>().sprite;
+        //        }
+        //        else
+        //        {
+        //            images[2].sprite = element.GetComponent<SpriteRenderer>().sprite;
+        //        }
+
+        //        Destroy(element.gameObject);
+        //    }
+        //    foreach (MachineEngine machine in FindObjectsOfType<MachineEngine>())
+        //    {
+        //        Destroy(machine.gameObject);
+        //    }
+        //    input.gameObject.SetActive(true);
+
+            
+        //}
+
+
+    }
+
+    public void SetImages()
+    {
+        foreach (ElementBehavior element in elements)
+        {
+            if (element.machine.name == "Slot Machine")
+            {
+                images[0].sprite = element.GetComponent<SpriteRenderer>().sprite;
+            }
+            else if (element.machine.name == "Slot Machine (1)")
+            {
+                images[1].sprite = element.GetComponent<SpriteRenderer>().sprite;
+            }
+            else
+            {
+                images[2].sprite = element.GetComponent<SpriteRenderer>().sprite;
+            }
+
+            Destroy(element.gameObject);
+        }
+        foreach (MachineEngine machine in FindObjectsOfType<MachineEngine>())
+        {
+            Destroy(machine.gameObject);
+        }
+        input.gameObject.SetActive(true);
+
+        SetInvention(inventions[FindObjectOfType<MachineEngine>().row]);
+        icon.sprite = icons[FindObjectOfType<MachineEngine>().row];
+
+    }
+
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(1);
+        
+    }
+
+    public int GetActiveScene()
+    {
+        return SceneManager.GetActiveScene().buildIndex;
+    }
+
+    public void SetInvention(string value)
+    {      
+        inventionName = value;
+        example.text = value;
+        
+    }
+
+    public void ShowPanel()
+    {
+        isShowing = !isShowing;
+        panel.SetActive(isShowing);
+    }
+}
